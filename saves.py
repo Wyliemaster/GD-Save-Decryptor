fb30f93e11dbaa5793e1c1a17abb16681ce1b7c4
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-16 18:36:47 +0100

    bugfix

--- saves.py
+++ saves.py
@@ -7,7 +7,7 @@ def xor(data: bytearray, key: int) -> bytearray:
     """
     Applies XOR Cipher onto data with a static key
     """
-    
+
     for i in range(len(data)):
         data[i] = data[i] ^ key
 
@@ -32,14 +32,13 @@ def decrypt_save(path: str) -> str:
         save = f.read()
 
     # If the save is a Mac/IOS save
-    if save[0] == 21:
-        cipher = AES.new(
-            b"\x69\x70\x75\x39\x54\x55\x76\x35\x34\x79\x76\x5d\x69\x73\x46\x4d\x68\x35\x40\x3b\x74\x2e\x35\x77\x33\x34\x45\x32\x52\x79\x40\x7b", AES.MODE_ECB)
-        return remove_pad(cipher.decrypt(save)).decode()
-
-    else:
-        save = bytearray(save) # Bytearrays are mutable therefore faster.
+    if save[0] == 67:
+        save = bytearray(save)
         b64 = xor(save, 0xB)
         zipped = base64.urlsafe_b64decode(b64)
         return zlib.decompress(zipped[10:], -zlib.MAX_WBITS).decode()
 
+    else:
+        cipher = AES.new(
+            b"\x69\x70\x75\x39\x54\x55\x76\x35\x34\x79\x76\x5d\x69\x73\x46\x4d\x68\x35\x40\x3b\x74\x2e\x35\x77\x33\x34\x45\x32\x52\x79\x40\x7b", AES.MODE_ECB)
+        return remove_pad(cipher.decrypt(save)).decode()
