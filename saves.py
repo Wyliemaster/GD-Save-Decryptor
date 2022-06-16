ecdbcc6a5cbae09313e1c6270408b5a8fd6b9fac
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-16 18:37:52 +0100

    Comments

--- saves.py
+++ saves.py
@@ -31,13 +31,14 @@ def decrypt_save(path: str) -> str:
     with open(path, "rb") as f:
         save = f.read()
 
-    # If the save is a Mac/IOS save
+    # Checking if the save is windows
     if save[0] == 67:
         save = bytearray(save)
         b64 = xor(save, 0xB)
         zipped = base64.urlsafe_b64decode(b64)
         return zlib.decompress(zipped[10:], -zlib.MAX_WBITS).decode()
 
+    # Otherwise assume the save is mac
     else:
         cipher = AES.new(
             b"\x69\x70\x75\x39\x54\x55\x76\x35\x34\x79\x76\x5d\x69\x73\x46\x4d\x68\x35\x40\x3b\x74\x2e\x35\x77\x33\x34\x45\x32\x52\x79\x40\x7b", AES.MODE_ECB)
